43f5c2d7ca5c6d301ff2a5db03c15c4b63875915
David Y. Kim <davidykim@gmail.com>
2016-10-04 18:30:04 -0400

    end of day 1

--- public/js/htmllogic.js
+++ public/js/htmllogic.js
@@ -1,14 +1,40 @@
-$(document).ready({
-  $("#hoteladdbtn").on("click", function (){
-    console.log(Object.keys($("select")[0]));
+$(document).ready(function () {
 
+  /* Data model:
+
+  Itinerary = [Day 1, Day 2, Day 3]
+
+  Day 1 = {
+    hotel:,
+    activity:,
+    restaurant
+  }
+
+  hotel: []
+  restaurant: [restaurant1, restaurant2 ...]
+  activity: []
+
+
+  */
+
+  var itinerary = []
+  var currentDay = 0;
+
+  // var temp = $(".hoteladdbtn");
+  // console.log("temp", temp);
+
+  $(".hoteladdbtn").on("click", function (){
+    var selectedHotel = $("#hotel-choices option:selected").text()
+    console.log(selectedHotel);
+
+    itinerary[currentDay] = {
+      hotel: selectedHotel
+      }
+
+
+    //   .hotel.push(selectedHotel);
+    // console.log("Hotel in array: ", itinerary[currentDay].hotel[0]);
 
   });
-
-
-
-
-
-
 
 });
