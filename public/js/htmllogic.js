eac81b23f49c13e0801ca22e3d1d7194a8f512ef
David Y. Kim <davidykim@gmail.com>
2016-10-05 10:07:30 -0400

    morning

--- public/js/htmllogic.js
+++ public/js/htmllogic.js
@@ -17,23 +17,26 @@ $(document).ready(function () {
 
   */
 
+//Initialize
   var itinerary = []
+  var emptyDay = {hotel: null, activity: null, restaurant:null}
   var currentDay = 0;
+  itinerary.push(emptyDay);
 
-  // var temp = $(".hoteladdbtn");
-  // console.log("temp", temp);
+//Event handling
 
   $(".hoteladdbtn").on("click", function (){
-    var selectedHotel = $("#hotel-choices option:selected").text()
+    var selectedHotel = $("#hotel-choices option:selected").text();
     console.log(selectedHotel);
 
-    itinerary[currentDay] = {
-      hotel: selectedHotel
-      }
+    itinerary[currentDay].hotel = selectedHotel;
 
 
     //   .hotel.push(selectedHotel);
-    // console.log("Hotel in array: ", itinerary[currentDay].hotel[0]);
+    console.log("Hotel in array: ", itinerary[currentDay].hotel);
+
+  // $(".hotel")
+
 
   });
 
